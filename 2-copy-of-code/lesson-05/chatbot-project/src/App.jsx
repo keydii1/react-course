e0f8d7a9609d6a019dbbefca30fa93d6a51ee327
Hồ Hoàng Sơn <23521352@gm.uit.edu.vn>
2025-09-19 12:18:36 +0700

    jj

--- 2-copy-of-code/lesson-05/chatbot-project/src/App.jsx
+++ 2-copy-of-code/lesson-05/chatbot-project/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { ChatInput } from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
 import './App.css'
-
 function App() {
   const [chatMessages, setChatMessages] = useState([{
     message: 'hello chatbot',
